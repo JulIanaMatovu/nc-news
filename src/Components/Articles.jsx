d845af3c8428b1ceb70e50ad6bea663fccbc85d7
JulIanaMatovu <80117718+JulIanaMatovu@users.noreply.github.com>
2022-07-26 10:26:40 +0100

    Refactored code - loading functionality added, key for parent render added, findCategory removed from Articles.jsx

--- src/Components/Articles.jsx
+++ src/Components/Articles.jsx
@@ -1,38 +1,11 @@
 import { useEffect, useState } from "react";
 import { fetchArticles } from "../API/api";
-import ArticleCard from "./ArticleCard";
-import { Link, Outlet } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 export default function Home() {
   const [articleData, setArticleData] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
-  const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
-
-  let image_url = "";
-
-  function findCategory(article) {
-    switch (article.topic) {
-      case "coding":
-        image_url =
-          "https://cdn.columbiauniversitybootcamp.com/wp-content/uploads/sites/108/2020/10/CDG_blog_post_image_02.jpg";
-        break;
-
-      case "cooking":
-        image_url = "https://img.rasset.ie/001c1b9f-600.jpg";
-        break;
-
-      case "football":
-        image_url =
-          "https://www.thefa.com/-/media/thefacom-new/images/england/womens-senior/world-cup/england-v-norway/england-800-celeb-norway-smile.ashx?as=0&dmc=0&thn=0";
-        break;
-
-      default:
-        image_url = "";
-        break;
-    }
-    return image_url;
-  }
-
+ 
   useEffect(() => {
     fetchArticles().then((data) => {
       setArticleData(data);
@@ -43,20 +16,26 @@ export default function Home() {
   return (
     <div>
       <h1>Articles</h1>
-      <div className="articleList">
-        {articleData.map((article) => (
-          <div className="articleName">
-            <Link to={`/articles/${article.article_id}`}>
+
+      {isLoading ? (
+        <div className="loadingMsg">
+          <h2>Loading Articles...</h2>
+        </div>
+      ) : (
+        <div className="articleList">
+          {articleData.map((article) => (
+            <div className="articleName" key={article.article_id}>
+              <Link to={`/articles/${article.article_id}`}>
                 <h6>{article.title}</h6>
-            </Link>
-            <Outlet />
-            <p>Author: {article.author}</p>
+              </Link>
+              <p>Author: {article.author}</p>
               <p>Votes: {article.votes}</p>
               <p>Date: {article.created_at}</p>
-            <hr></hr>
-          </div>
-        ))}
-      </div>
+              <hr></hr>
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 }
